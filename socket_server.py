2e9a422def90f3bbe51fe2dc6370a3744fff15fc
fuqiang.zhu <fuqiang.zhu@godinsec.com>
2017-04-20 22:09:04 +0800

    socket_server

--- socket_server.py
+++ socket_server.py
@@ -1,13 +1,21 @@
+#
+# import socket
+#
+# server = socket.socket()
+# server.bind(('localhost', 5050))
+# server.listen()
+# conn, addr = server.accept()
+# print(conn, addr)
+# data = conn.recv(1024)
+# print(data)
+# conn.send(data.upper())
+#
+# server.close()
+#
+flag = 10
+while flag:
+    print('--')
+    flag -= 1
+else:
+    print('====')
 
-import socket
-
-server = socket.socket()
-server.bind(('localhost', 5050))
-server.listen()
-conn, addr = server.accept()
-print(conn, addr)
-data = conn.recv(1024)
-print(data)
-conn.send(data.upper())
-
-server.close()
